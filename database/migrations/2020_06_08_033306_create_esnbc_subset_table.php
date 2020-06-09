e02d6d4520adb732214d6bc176d5aa062a76531b
Shen Yuan Zhe <58937356+forwardman1993@users.noreply.github.com>
2020-06-09 05:32:48 +0300

    before admin panel

---
+++ database/migrations/2020_06_08_033306_create_esnbc_subset_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEsnbcSubsetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('esnbc_subsets', function (Blueprint $table) {
+            $table->id();
+            $table->string('batches_info_id', 100)->references('batches_info')->on('esnbcs');
+            $table->date('production_date');
+            $table->date('best_before_date')->nullable();
+            $table->float('net_weight', 8, 2)->nullable();
+            $table->string('dimensions', 100)->nullable();
+            $table->string('customer_purchase_order_number', 100)->nullable();
+            $table->string('shipment_tracking_number', 100)->nullable();
+            $table->boolean('is_updated_to_fabric')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('esnbc_subsets');
+    }
+}
