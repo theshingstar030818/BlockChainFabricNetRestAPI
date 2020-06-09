98cf3f6f8f2c54e39f4f61535281eabfe9d83a36
Shen Yuan Zhe <58937356+forwardman1993@users.noreply.github.com>
2020-06-09 13:05:30 +0300

    before creating admin

---
+++ app/Esnbc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Esnbc extends Model
+{
+    protected $table = 'esnbcs';
+    protected $primaryKey = 'batches_info';
+}
---
+++ app/EsnbcSubset.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EsnbcSubset extends Model
+{
+    protected $table = 'esnbc_subsets';
+    protected $primaryKey = 'id';
+}
