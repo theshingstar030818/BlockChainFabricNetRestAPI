8029be14ad419e6beb4f7e9f5d6a0a018e368f49
Shen Yuan Zhe <58937356+forwardman1993@users.noreply.github.com>
2020-06-17 06:38:57 +0300

    updated user seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,8 +14,8 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->delete();
 
-        User::create(['name' => 'admin', 'email' => 'guruilya86@gmail.com', 'password' => bcrypt('admin123'), 'is_enabled' => true, 'is_admin' => true]);
-        User::create(['name' => 'user01', 'email' => 'fwd2018man@mail.ru', 'password' => bcrypt('user01123'), 'is_enabled' => true, 'is_admin' => false]);
+        User::create(['name' => 'admin', 'email' => 'admin@gmail.com', 'password' => bcrypt('admin123'), 'is_enabled' => true, 'is_admin' => true]);
+        User::create(['name' => 'user01', 'email' => 'user01@mail.ru', 'password' => bcrypt('user01123'), 'is_enabled' => true, 'is_admin' => false]);
         User::create(['name' => 'user02', 'email' => 'user02@gmail.com', 'password' => bcrypt('user02123'), 'is_enabled' => true, 'is_admin' => false]);
     }
 }
