7185ab081bb599d00468a02f9940a1cb37e400ee
Shen Yuan Zhe <58937356+forwardman1993@users.noreply.github.com>
2020-06-19 00:27:56 +0300

    added & updated logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,8 +25,9 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container nav-container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img alt="Logo Image" src="{{ asset('images/logo.png') }}" class="logo-img">
                 </a>
+                <span class="logo-text">{{ config('app.name', 'Laravel') }}</span>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
